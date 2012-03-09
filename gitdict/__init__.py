5840164b7595436c1bc84f8d279cba387ed9a0f3
John Krauss <irving.krauss@gmail.com>
2012-03-09 17:51:59 -0500

    bumping to 0.0.3

--- gitdict/__init__.py
+++ gitdict/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
 __title__ = 'gitdict'
-__version__ = '0.0.2'
-__build__ = 0x000002
+__version__ = '0.0.3'
+__build__ = 0x000003
 __author__ = 'John Krauss'
 __license__ = 'BSD'
 __copyright__ = 'Copyright 2012 John Krauss'
